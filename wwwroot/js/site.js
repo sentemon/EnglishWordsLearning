2c94cd5e8c562f437ac1f06f2f2d2b09f101621e
Ivan Sentemon <sentemon.i@gmail.com>
2024-06-19 13:15:42 +0200

    feat: Add .DS_Store to .gitignore and update jQuery and Bootstrap dependencies
    This commit adds .DS_Store to the .gitignore file to prevent it from being tracked by Git. It also updates the jQuery and Bootstrap dependencies in the _Layout.cshtml file to use local versions instead of CDN links. These changes improve the project's file management and reduce external dependencies.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,15 +4,15 @@
 // Write your JavaScript code.
 
 
-
 // This function sets focus on the userTranslation input field
 function setFocus() {
     document.getElementById("userTranslation").focus();
 }
+
 // Set focus when the page loads
 window.onload = setFocus;
 
 // Set focus after the form is submitted
-document.getElementById("translationForm").onsubmit = function() {
-    setTimeout(setFocus); // Slight delay to ensure the form submission is processed first
+document.getElementById("translationForm").onsubmit = function () {
+    setTimeout(setFocus);
 };
