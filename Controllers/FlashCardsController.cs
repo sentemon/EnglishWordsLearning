dee6a3016b831a67c9466dca5efde4fd8764064d
Ivan Sentemon <sentemon.i@gmail.com>
2024-07-13 12:56:45 +0200

    Remove unused code

--- Controllers/FlashCardsController.cs
+++ Controllers/FlashCardsController.cs
@@ -1,24 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using EnglishWordsLearning.Services;
-using EnglishWordsLearning.Models;
 
 namespace EnglishWordsLearning.Controllers;
 
 public class FlashCardsController : Controller
 {
     
-
     public async Task<IActionResult> Index(string level = "AllLevels")
     {
         var words = await LoadWordsHelper.LoadCsvWordsAsync(ViewData, level);
         
         var randomWord = LoadWordsHelper.GetRandomWord(words);
-        
-        int totalQuestions = HttpContext.Session.GetInt32("totalQuestions") ?? 0;
-        HttpContext.Session.SetInt32("totalQuestions", totalQuestions);
-        
-        ViewBag.TotalQuestions = totalQuestions;
-        totalQuestions++; // doesn't work
         
         ViewBag.DisplayedLevel = LoadWordsHelper.Levels[level];
         ViewBag.SelectedLevel = level;
